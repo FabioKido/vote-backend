2297c8a9dac9bd944187b14df66c0c90754a532f
FabioKido <fabiohenryquemesquita@gmail.com>
2023-11-12 20:51:06 -0300

    chore(Server): Add cache support with redis

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const { createClient } = require('redis')
 
 const router = require('./routes')
 
 const PORT = process.env.PORT || 3000
 const app = express()
+const client = createClient()
 
 
 app.use(express.json())
 app.use('/api', router)
 
-app.listen(PORT, () => console.log('Server is running...'))
+const startServer = async() => {
+    await client.connect()
+    
+    app.listen(PORT, () => console.log('Server is running...'))
+}
+
+startServer()
